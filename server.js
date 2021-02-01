5ee87de2a8522f7977c4d196646965317962f312
Damir <dkolobaric@gmail.com>
2021-02-01 17:50:41 +0100

    adding express

--- server.js
+++ server.js
@@ -1,13 +1,62 @@
 const http = require('http');
+const app = require('./app');
+const debug = require('debug')('node-angular');
 
+const normalizePort = val => {
+
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+
+  if (error.syscall !== "listen") {
+    throw error;
+  };
+
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
 const hostname = '127.0.0.1';
-const port = 3000;
+app.set('port', port);
+app.set('hostname', hostname)
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
